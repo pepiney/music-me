936666419befa62a251a2558610c251394da2268
Christophe Pala <pala.christophe@gmail.com>
2020-11-25 13:11:36 +0100

    showRooms

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,27 @@
 class ReservationsController < ApplicationController
+
+  def create
+    @room = Room.find(params[:room_id])
+    @reservation = Reservation.new(resa_params)
+    @reservation.room = @room
+    if @reservation.save
+    redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to rooth_path(@reservation.room)
+  end
+
+  private
+
+  def resa_params
+      params.require(:reservation).permit(:description, :date)
+  end
+
 end
+
