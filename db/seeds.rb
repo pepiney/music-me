487ca50767ecce7b5498aaa4f5368eedbb04c8aa
mathieu fauvette <fauvettemathieu.mf@gmail.com>
2020-11-25 16:20:47 +0100

    aller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user1 = User.create(email: "jean2@gmail.com", password: "123456")
-Room.create!(name: "Studio Vauban", city: "Marseille", user: user1, user_id: 1, photo: "http://www.levip-saintnazaire.com/wp-content/uploads/2016/08/VIP-studio-2-%C2%A9-PUK-Samia-Hamlaoui-768x512.jpg")
+Room.create(name: "Studio Vauban", city: "Marseille", user: user1, user_id: 1, photo: "https://www.pepsup.com/resources/images/ARTICLES/000/007/125/71257/IMAGE/71257.jpg?1574964143000")
 Room.create(name: "Studio Endoume", city: "Marseille", user: user1, user_id: 1, photo: "https://octavoxstudio.com/wp-content/uploads/2016/12/repetition1920x610.jpg")
 Room.create(name: "Studio Porte d'Aix", city: "Marseille", user: user1, user_id: 1, photo: "https://labodessons.fr/wp-content/uploads/2019/01/LDS%C2%A9LEOFAVREAU-4-1280x720.jpg")
 Room.create(name: "Studio La Viste", city: "Marseille", user: user1, user_id: 1, photo: "https://i.pinimg.com/originals/eb/c9/8b/ebc98b4918c76b72b41f590b137ca613.jpg")
